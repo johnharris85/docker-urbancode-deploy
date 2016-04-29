b750177f61e69097460335cc61994a4cf474ed6a
johnharris85 <john@johnharris.io>
2016-04-29 18:38:52 -0400

    Fix indentation

--- initial_config.py
+++ initial_config.py
@@ -5,17 +5,17 @@ import zipfile
 import os
 
 def move_server():
-	shutil.move('ibm-ucd-install', 'server')
+    shutil.move('ibm-ucd-install', 'server')
 
 def extract_agent():
-	shutil.copy('ibm-ucd-install/overlay/opt/tomcat/webapps/ROOT/tools/ibm-ucd-agent.zip', 'agent')
-	agent_zip = zipfile.ZipFile('agent/ibm-ucd-agent.zip', 'r')
-	agent_zip.extractall('agent')
-	agent_zip.close()
-	os.remove('agent/ibm-ucd-agent.zip')
+    shutil.copy('ibm-ucd-install/overlay/opt/tomcat/webapps/ROOT/tools/ibm-ucd-agent.zip', 'agent')
+    agent_zip = zipfile.ZipFile('agent/ibm-ucd-agent.zip', 'r')
+    agent_zip.extractall('agent')
+    agent_zip.close()
+    os.remove('agent/ibm-ucd-agent.zip')
 
 if __name__ == "__main__":
-	extract_agent()
-	move_server()
-	print "Files copied..."
-	print "Now run ... $ docker-compose up"
+    extract_agent()
+    move_server()
+    print "Files copied..."
+    print "Now run ... $ docker-compose up"
